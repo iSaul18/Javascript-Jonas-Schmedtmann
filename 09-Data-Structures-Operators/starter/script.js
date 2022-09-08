92c674f610a9c5a00278774b87e46c747e07efe6
iSaul18 <porrasemmiliosaul@gmail.com>
2022-09-07 21:27:39 -0500

    optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -269,7 +269,7 @@
 // restaurant.orderPizza('mushorrom', 'onion', 'olives', 'espinach');
 // restaurant.orderPizza('mushorrom');
 
-// // 
+// //
 // --------------------------- && - || OPERATORS LOGICS
 // Use ANY data type, return ANY data type, shor-circuiting
 
@@ -308,7 +308,7 @@
 // const guests2 = restaurant.numGuests || 10;
 // console.log(guests2);
 
-// && AND 
+// && AND
 // let a = 10;
 // let b = 20;
 // console.log(10 && 0);
@@ -354,8 +354,6 @@
 // rest2.numGuets ??= 10;
 
 // rest2.owner &&= 'jonas';
-
-
 
 // console.log(rest1);
 // console.log(rest2);
@@ -414,7 +412,7 @@
 //        'Sancho',
 //        'Gotze',
 //      ],
-//    ], 
+//    ],
 //    score: '4:0',
 //    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
 //    date: 'Nov 9th, 2037',
@@ -427,7 +425,7 @@
 // //  1
 // let [players1, players2] = game.players
 // // 2
-// let [gk, ...fieldPlayers] = game.players[0]; 
+// let [gk, ...fieldPlayers] = game.players[0];
 // // 3
 // let allPlayers = [...players1, ...players2];
 // // 4
@@ -445,3 +443,112 @@
 // equipo2 === equipo1 && console.log('Puede ser empate');
 
 // ----------------------------------------------------
+// const restaurant = {
+
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   order: function (starter, main) {
+//     return [this.starterMenu[starter], this.mainMenu[main]];
+//   },
+
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(`Here is delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+//   },
+
+//   orderPizza: function(mainIngre, ...otherIngre) {
+//     console.log(`${mainIngre}`);
+//     console.log(`${otherIngre}`);
+//   },
+// }
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// // for (const item of menu) console.log(item);
+
+// menu.forEach((x,i) => console.log(`${i+1}: ${x}`));
+// console.log('--------------------');
+// // let indice = 0;
+// // for (let items of arreglo) {
+// //   console.log(items);
+// //   indice++;
+// //   console.log(indice);
+// // };
+
+// for (const item of menu.entries()) {
+//   console.log(`${item[0]+1}: ${item[1]}`);
+// }
+// console.log('---------------Se puede destructurar en un for of');
+//  for (const [i, el] of menu.entries()) {
+//    console.log(`${i +1}: ${el}`);
+//  }
+
+// // --------------------------------------------------
+// // OBJETCS LITERALS------------------------------------
+
+const openingHours = {
+  //   // OPERACIONES CON PLANTILLAS LITERALES
+  fri: {
+    open: 12,
+    close: 22,
+  },
+  thu: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 Enhancet Objetc Literals
+  // Solo poner el nombre de la variable
+  openingHours,
+
+  // Forma moderna de declarar funcinoes dentro de un objeto
+  orderPizza(mainIngre, ...otherIngre) {
+    console.log(`${mainIngre}`);
+    console.log(`${otherIngre}`);
+  },
+
+  order: function (starter, main) {
+    return [this.starterMenu[starter], this.mainMenu[main]];
+  },
+};
+
+// OPTIONAL CHAINGING
+console.log(restaurant.openingHours.mon);
+
+// WITH
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours.sat?.open);
+console.log(restaurant.openingHours?.mon?.close);
+
+// EXAMPLE
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  const close = restaurant.openingHours[day]?.close;
+  console.log(`On ${day}, we Open at: ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? ' Method does not exist');
+console.log(restaurant.ordefar?.(0, 1) ?? ' Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(users[0]?.name ?? 'User array empty');
+// -------------------------------------------------------
