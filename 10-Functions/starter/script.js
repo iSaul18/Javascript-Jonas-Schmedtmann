302c31eef77ab64e8e84f375a2df662c5700fcc2
iSaul18 <porrasemmiliosaul@gmail.com>
2022-09-12 20:45:03 -0500

    data-structures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const bookings = [];
+
+const createBooking = function (fligthNum = 1, numPassengers = 1, price = 199) {
+    const booking = {
+      fligthNum,
+      numPassengers,
+      price,
+    };
+
+    console.log(booking);
+    bookings.push(booking)
+}
+
+createBooking('LH123')
+createBooking('LH125', 2, 800)
